f1b3c0d8a274df00b5428da4f7ab853cebb7632a
ahmetdevserefoglu <ahmet@tribe.us>
2023-10-28 03:13:33 +0300

    fix: eureka gateway load balancing

--- inventory-service/src/main/java/com/microservis/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/microservis/inventoryservice/service/InventoryService.java
@@ -1,17 +1,29 @@
 package com.microservis.inventoryservice.service;
 
+import com.microservis.inventoryservice.dto.InventoryResponse;
 import com.microservis.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class InventoryService
 {
+    /*
+    * Servisin içerisinde repository oluşturulması istenmiyor best practies değil
+    * */
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-       return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+       return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+               .map(inventory ->
+                   InventoryResponse.builder()
+                           .skuCode(inventory.getSkuCode())
+                           .isInStock(inventory.getQuantity()>0)
+                           .build()
+               ).toList();
     }
 }
--- order-service/src/main/java/com/microservis/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservis/orderservice/service/OrderService.java
@@ -2,13 +2,16 @@ package com.microservis.orderservice.service;
 
 import com.microservis.orderservice.dto.OrderLineItemsDto;
 import com.microservis.orderservice.dto.OrderRequest;
+import com.microservis.orderservice.model.InventoryResponse;
 import com.microservis.orderservice.model.Order;
 import com.microservis.orderservice.model.OrderLineItems;
 import com.microservis.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final WebClient.Builder webClientBuilder;
+
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -29,9 +35,29 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+        // CAll Inventory Service and place order if product is in
+        // stock
+       InventoryResponse[] inventoryResponsesArray =  webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                        .retrieve()
+                                .bodyToMono(InventoryResponse[].class)
+                                        .block();
+       boolean allProductsInStock =  Arrays.stream(inventoryResponsesArray)
+               .allMatch(inventoryResponse -> inventoryResponse.isInStock());
+
+       if (allProductsInStock){
+           orderRepository.save(order);
+       }else{
+           throw new IllegalArgumentException("Product is not in stock, please try again later");
+       }
+
     }
 
+    /*
+    * Genel ModelMapper oluşturuyoruz */
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
