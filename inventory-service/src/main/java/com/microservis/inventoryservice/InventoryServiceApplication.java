4dcd60a73fc2ee4da30e7213a8bf95177e043b25
ahmetdevserefoglu <ahmet@tribe.us>
2023-10-29 03:06:53 +0300

    fix: eureka

--- inventory-service/src/main/java/com/microservis/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservis/inventoryservice/InventoryServiceApplication.java
@@ -5,10 +5,12 @@ import com.microservis.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+@EnableDiscoveryClient
+public class InventoryServiceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
--- inventory-service/src/main/java/com/microservis/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/microservis/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.microservis.inventoryservice.controller;
 
+import com.microservis.inventoryservice.dto.InventoryResponse;
 import com.microservis.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/inventory")
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
 
     private final InventoryService inventoryService;
-    @GetMapping("/{sku-code}")
+
+    // http://localhost:8082/api/inventory/iphone-13,ipnhone13-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
+
         return  inventoryService.isInStock(skuCode);
     }
 }
